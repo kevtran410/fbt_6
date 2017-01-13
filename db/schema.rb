c2fa0ffc75b4d777bbd24c4c8dab699aa5e46adf
Tran Ngoc Nam <tran.ngoc.nam@framgia.com>
2017-01-13 10:37:32 +0700

    show tour in home page

--- db/schema.rb
+++ db/schema.rb
@@ -150,6 +150,29 @@ ActiveRecord::Schema.define(version: 20170117030916) do
     t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
   end
 
+  create_table "rates", force: :cascade do |t|
+    t.integer "rater_id"
+    t.string "rateable_type"
+    t.integer "rateable_id"
+    t.float "stars", null: false
+    t.string "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
+    t.index ["rater_id"], name: "index_rates_on_rater_id"
+  end
+
+  create_table "rating_caches", force: :cascade do |t|
+    t.string "cacheable_type"
+    t.integer "cacheable_id"
+    t.float "avg", null: false
+    t.integer "qty", null: false
+    t.string "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.string "name"
     t.text "content"
@@ -186,6 +209,7 @@ ActiveRecord::Schema.define(version: 20170117030916) do
     t.integer "duration"
     t.float "price"
     t.float "rating"
+    t.text "content"
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
